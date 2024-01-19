3ac5c1fd17cc848b791c257ffd600416fe257392
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2024-01-19 15:06:03 +0700

    Release V.1.6.7 fix webhook

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -1,43 +1,107 @@
-
-import crypto from 'crypto'
+import crypto from "crypto";
 
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+class TransactionService {
+  async AddCourseToUser(getOrderId) {
+    return prisma.purchase.create({
+      data: {
+        userId: getOrderId.userId,
+        courseId: getOrderId.courseId,
+      },
+    });
+  }
+
+  async UpdateCart(order_id) {
+    return prisma.cart.updateMany({
+      where: {
+        orderId: order_id,
+      },
+      data: {
+        isPaid: true,
+      },
+    });
+  }
+
+  async getOrderIdbyCart(order_id) {
+    const getOrderId = await prisma.cart.findFirst({
+      where: {
+        orderId: order_id,
+      },
+    });
+
+    return {
+      getOrderId,
+    };
+  }
+}
+
+const transactionService = new TransactionService();
+
+const checkSignature = async (data) => {
+  const { MIDTRANS_SERVERKEY_DEV } = process.env;
+
+  const hash = crypto
+    .createHash("sha512")
+    .update(
+      data.order_id +
+        data.status_code +
+        data.gross_amount +
+        MIDTRANS_SERVERKEY_DEV
+    )
+    .digest("hex");
+
+  if (data.signature_key !== hash) {
+    return {
+      status: "error",
+      message: "Invalid Signature Key",
+    };
+  }
+};
+
 export const createPayment = async (req, res) => {
-    const { order_id, transaction_status, signature_key, gross_amount, status_code } = req.body
+  // const { order_id, transaction_status, signature_key, gross_amount, status_code } = req.body
 
-    const{ MIDTRANS_SERVERKEY_DEV } = process.env  
+  const data = req.body;
 
-        const hash = crypto.createHash('sha512').update(order_id + status_code + gross_amount + MIDTRANS_SERVERKEY_DEV).digest('hex')
+  console.log(data);
 
-        if (signature_key !== hash )  return res.status(401).json({'error': 'signature mismatch'})
+  await checkSignature(data);
 
-        const getOrderId = await prisma.cart.findFirst({
-            where: {
-                orderId: order_id
-            }
-        })
+  const getOrderId = await prisma.cart.findFirst({
+    where: {
+      orderId: data.order_id,
+    },
+  });
 
-        if (!getOrderId) return res.status(401).json({'no success': 'we not found your order id'})
+  console.log(getOrderId);
 
-        await prisma.purchase.create({
-            data: {
-                userId,
-                courseId: getOrderId.courseId
-            }
-        })
+  // Add Course to User
+  if (getOrderId) {
+    const AddUserToCourse = await prisma.purchase.create({
+      data: {
+        userId: getOrderId.userId,
+        courseId: getOrderId.courseId,
+      },
+    });
 
-        await prisma.cart.updateMany({
-            where: {
-                order_id
-            },
-            data: {
-                isPaid: true,
-            }
-        })
+    console.log(AddUserToCourse);
+  }
 
-        res.status(201).json({'success': 'Purchase added successfully'})
-   
-} 
+  //Update Status Card
+  await prisma.cart.updateMany({
+    where: {
+      orderId: data.order_id,
+    },
+    data: {
+      isPaid: true,
+    },
+  });
+
+  res.status(200).json({
+    status: "Success",
+    message: "OK",
+  });
+};
