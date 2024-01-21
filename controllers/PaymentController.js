5daff59f72bd8335d798e7b6f7e0b4b03b7f2c63
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2024-01-21 20:13:40 +0700

    Release with fix 1.6.8 webhook fix

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -6,16 +6,20 @@ const prisma = new PrismaClient();
 
 class TransactionService {
   async AddCourseToUser(getOrderId) {
-    return prisma.purchase.create({
+    const data = await prisma.purchase.create({
       data: {
         userId: getOrderId.userId,
         courseId: getOrderId.courseId,
       },
     });
+
+    return {
+      data,
+    };
   }
 
   async UpdateCart(order_id) {
-    return prisma.cart.updateMany({
+    return await prisma.cart.updateMany({
       where: {
         orderId: order_id,
       },
@@ -61,6 +65,44 @@ const checkSignature = async (data) => {
   }
 };
 
+const handleAddCourseToUser = async (order_id) => {
+  const getOrderId = await prisma.cart.findFirst({
+    where: {
+      orderId: order_id,
+    },
+  });
+  console.log("getOrderId:", getOrderId);
+
+  const isDuplicate = await prisma.purchase.findUnique({
+    where: {
+      courseId: getOrderId.courseId,
+    },
+  });
+
+  console.log("checkDuplicate:", isDuplicate);
+
+  //check duplicate
+  if (!isDuplicate) {
+    // Add Course to User
+    const AddUserToCourse = await prisma.purchase.create({
+      data: {
+        userId: getOrderId.userId,
+        courseId: getOrderId.courseId,
+      },
+    });
+    console.log(AddUserToCourse);
+    //Update Status Card
+    await prisma.cart.updateMany({
+      where: {
+        orderId: order_id,
+      },
+      data: {
+        isPaid: true,
+      },
+    });
+  }
+};
+
 export const createPayment = async (req, res) => {
   // const { order_id, transaction_status, signature_key, gross_amount, status_code } = req.body
 
@@ -70,35 +112,30 @@ export const createPayment = async (req, res) => {
 
   await checkSignature(data);
 
-  const getOrderId = await prisma.cart.findFirst({
-    where: {
-      orderId: data.order_id,
-    },
-  });
+  let orderId = data.order_id;
+  let transactionStatus = data.transaction_status;
+  let fraudStatus = data.fraud_status;
 
-  console.log(getOrderId);
-
-  // Add Course to User
-  if (getOrderId) {
-    const AddUserToCourse = await prisma.purchase.create({
-      data: {
-        userId: getOrderId.userId,
-        courseId: getOrderId.courseId,
-      },
-    });
-
-    console.log(AddUserToCourse);
+  if (transactionStatus == "capture" || transactionStatus == "settlement") {
+    if (fraudStatus == "accept") {
+      // TODO set transaction status on your database to 'success'
+      // and response with 200 OK
+      await handleAddCourseToUser(orderId);
+    }
+  } else if (transactionStatus == "settlement") {
+    // TODO set transaction status on your database to 'success'
+    // and response with 200 OK
+  } else if (
+    transactionStatus == "cancel" ||
+    transactionStatus == "deny" ||
+    transactionStatus == "expire"
+  ) {
+    // TODO set transaction status on your database to 'failure'
+    // and response with 200 OK
+  } else if (transactionStatus == "pending") {
+    // TODO set transaction status on your database to 'pending' / waiting payment
+    // and response with 200 OK
   }
-
-  //Update Status Card
-  await prisma.cart.updateMany({
-    where: {
-      orderId: data.order_id,
-    },
-    data: {
-      isPaid: true,
-    },
-  });
 
   res.status(200).json({
     status: "Success",
--- controllers/PurchaseController.js
+++ controllers/PurchaseController.js
@@ -1,98 +1,99 @@
-import express from "express";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export const getPurchase = async (req, res) => {
-    try {
-        const response = await prisma.purchase.findMany({
-            include: {
-                courses: true
-            }
-        })
-        res.status(200).json(response)
-    } catch (error) {
-        res.status(404).json({ error: error })
-    }
-}
+  try {
+    const response = await prisma.purchase.findMany({
+      include: {
+        courses: true,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+};
 
 export const getPurchaseById = async (req, res) => {
-    const { userId } = req.params
-    try {
-        const response = await prisma.purchase.findMany({
-            where: {
-                userId
-            },
-            include: {
-                courses: true
-            }
-        })
-        res.status(200).json(response)
-    } catch (error) {
-        res.status(404).json({ error: 'Not Found' })
-    }
-}
+  const { userId } = req.params;
+  try {
+    const response = await prisma.purchase.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        courses: true,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(404).json({ error: "Not Found" });
+  }
+};
 
 export const createPurchase = async (req, res) => {
-    const { userId, orderId } = req.body 
-    try {
-        
-        const getOrderId = await prisma.cart.findFirst({
-            where: {
-                orderId: orderId
-            }
-        })
+  const { userId, orderId } = req.body;
+  try {
+    const getOrderId = await prisma.cart.findFirst({
+      where: {
+        orderId: orderId,
+      },
+    });
 
-        if (!getOrderId) return res.status(401).json({'no success': 'we not found your order id'})
+    if (!getOrderId)
+      return res
+        .status(401)
+        .json({ "no success": "we not found your order id" });
 
-        await prisma.purchase.create({
-            data: {
-                userId,
-                courseId: getOrderId.courseId
-            }
-        })
+    await prisma.purchase.create({
+      data: {
+        userId,
+        courseId: getOrderId.courseId,
+      },
+    });
 
-        await prisma.cart.updateMany({
-            where: {
-                orderId
-            },
-            data: {
-                isPaid: true,
-            }
-        })
+    await prisma.cart.updateMany({
+      where: {
+        orderId,
+      },
+      data: {
+        isPaid: true,
+      },
+    });
 
-        res.status(201).json({'success': 'Purchase added successfully'})
-    } catch (error) {
-        res.status(401).json({'error': error})
-    }
-}
+    res.status(201).json({ success: "Purchase added successfully" });
+  } catch (error) {
+    res.status(401).json({ error: error });
+  }
+};
 
 export const updatePurchase = async (req, res) => {
-    const { courseId } = req.params
-    try {
-        await prisma.purchase.update({
-            where: { courseId },
-            data: {
-                userId,
-                courseId
-            }
-        })
-        res.status(201).json({'success': 'Purchase Update successfully'})
-    } catch (error) {
-        res.status(401).json({'error': error})
-    }
-}
+  const { courseId } = req.params;
+  try {
+    await prisma.purchase.update({
+      where: { courseId },
+      data: {
+        userId,
+        courseId,
+      },
+    });
+    res.status(201).json({ success: "Purchase Update successfully" });
+  } catch (error) {
+    res.status(401).json({ error: error });
+  }
+};
 
 export const deletePurchase = async (req, res) => {
-    const { id } = req.params
-    try {
-        await prisma.purchase.delete({
-            where: {
-                id
-            }
-        })
-        res.status(201).json({'success': 'Purchase Deleted successfully'})
-    } catch (error) {
-        res.status(401).json({'error': error})
-    }
-}
+  const { id } = req.params;
+  try {
+    await prisma.purchase.delete({
+      where: {
+        id,
+      },
+    });
+    res.status(201).json({ success: "Purchase Deleted successfully" });
+  } catch (error) {
+    res.status(401).json({ error: error });
+  }
+};
