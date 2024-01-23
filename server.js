7658cd4ea7bee55e39a240a5dbcd2d4db74676b5
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2024-01-23 20:39:39 +0700

    Release V1.6.9 add limiter

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
-import CourseRoute from "./routes/CourseRoute.js"
-import CategoryRoute from "./routes/CategoryRoute.js"
-import PurchaseRoute from "./routes/PurchaseRoute.js"
-import CartRoute from "./routes/CartRoute.js"
-import ChapterRoute from "./routes/ChapterRoute.js"
-import AttachmentRoute from "./routes/AttachmentRoute.js"
-import PaymentRoute from "./routes/PaymentRoute.js"
-import UserRoute from "./routes/UserRoute.js"
-import CaptureRoute from "./routes/CaptureRoute.js"
-import PointRoute from "./routes/PointRoute.js"
-import VidiosRoute from "./routes/VidiosRoute.js"
-import ToolRoute from "./routes/ToolRoute.js"
-import InstrumentRoute from "./routes/IntrumentRoute.js"
+import CourseRoute from "./routes/CourseRoute.js";
+import CategoryRoute from "./routes/CategoryRoute.js";
+import PurchaseRoute from "./routes/PurchaseRoute.js";
+import CartRoute from "./routes/CartRoute.js";
+import ChapterRoute from "./routes/ChapterRoute.js";
+import AttachmentRoute from "./routes/AttachmentRoute.js";
+import PaymentRoute from "./routes/PaymentRoute.js";
+import UserRoute from "./routes/UserRoute.js";
+import CaptureRoute from "./routes/CaptureRoute.js";
+import PointRoute from "./routes/PointRoute.js";
+import VidiosRoute from "./routes/VidiosRoute.js";
+import ToolRoute from "./routes/ToolRoute.js";
+import InstrumentRoute from "./routes/IntrumentRoute.js";
 import { verifyToken } from "./middleware/verifyToken.js";
-
 
 dotenv.config();
 const { PORT } = process.env;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: "to many requests",
+});
+
 const app = express();
 // middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(helmet())
-app.disable('x-powered-by')
+app.use(helmet());
+app.disable("x-powered-by");
+app.use(limiter);
 
 app.use(CourseRoute);
 app.use(CategoryRoute);
@@ -35,7 +42,7 @@ app.use(PurchaseRoute);
 app.use(CartRoute);
 app.use(ChapterRoute);
 app.use(AttachmentRoute);
-app.use(PaymentRoute );
+app.use(PaymentRoute);
 app.use(UserRoute);
 app.use(CaptureRoute);
 app.use(PointRoute);
@@ -43,19 +50,10 @@ app.use(VidiosRoute);
 app.use(ToolRoute);
 app.use(InstrumentRoute);
 
-app.get('/test', verifyToken, (req, res) => {
+app.get("/test", verifyToken, (req, res) => {
   res.status(200).json(`hello I'm from port :` + PORT);
 });
 
-
-app.listen( PORT, () => {
-    console.log('listening on port ' + PORT);
-})
-
-
-
-
-
-
-
-
+app.listen(PORT, () => {
+  console.log("listening on port " + PORT);
+});
