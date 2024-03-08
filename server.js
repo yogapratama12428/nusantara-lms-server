7c6c224736e7fdcd962ae2359bf60be5a28a77e5
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2024-03-08 14:45:04 +0700

    R 2.5.3

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     limit: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    headers: true,
+    headers: false,
     standardHeaders: 'draft-7',
     legacyHeaders: false,
     validate: {
