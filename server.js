18297a126056eb717d42fe320235715acaa13112
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2024-03-07 23:13:24 -0800

    Release Note 2.4

--- server.js
+++ server.js
@@ -44,7 +44,12 @@ app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     limit: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    message: "to many requests",
+    headers: true,
+    handler: function (req, res, next) {
+      res.status(429).json({
+        message: "Too many requests, please try again later.",
+      });
+    },
   })
 );
 
